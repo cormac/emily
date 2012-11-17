ebd724793855636f30959356a2441c0969ee4c8f
cormac <cormac@studiorua.ie>
2012-11-17 21:03:18 +0000

    added images and audio

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,17 @@ ED.start = ( function ( document, window, undefined ) {
     console.log( 'load' );
     ED.sockets.startSockets ();
     ED.easel.startPainting ();
+    setTimeout( function () {
+      $( '#emilymad1' ).addClass('showEmily');
+      $( '#emilymad1' ).show();
+      $( '#gameCanvas ').hide();
+      var sound = new Audio('./sound/EmilyIsMad.mp3');
+      sound.play();
+      ee.emitEvent( 'gotFeather', [attack] );
+    }, 42000);
   };
   document.addEventListener("DOMContentLoaded", load, false);
+
+  
 
 } ( document, window ) );
